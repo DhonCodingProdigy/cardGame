464835d798b34c81977146303c333e1be7edf7e7
DhonCodingProdigy <dhon@mscodingacademies.org>
2024-02-02 01:38:20 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,140 @@
+const classType = [
+    'support',
+    'recon',
+    'skirmisher',
+    'assault',
+    'controller'
+]
+
+// Create menu items arr of objects
+/*
+Ash - Assault
+Bangalore - Assault
+ballistic - Assault
+fuse - assault
+mad maggie - assault
+horizon - skirmisher
+wraith - skirmisher
+octane - skirmisher
+pathfinder - skirmisher
+revenant - skirmisher
+valkyrie - skirmisher
+bloodhound - recon
+seer - recon
+crypto - recon
+vantage - recon
+catalyst - controller
+caustic - controller
+rampart - controller
+wattson - controller
+conduit - support
+lifeline - support
+gibraltar - support
+loba - support
+mirage -support
+*/
+let legendCards = [
+{
+    id: 1,
+    class: 'assault',
+    character: 'ash',
+    desc: 'Incisive Instigator',
+    imgUrl: 'ash.jpg',
+    }, 
+{
+    id: 2,
+    class: 'assault',
+    character: 'bangalore',
+    desc: 'Professional Soldier',
+    imgUrl: 'bangalore.jpg',
+}, 
+{
+    id: 3,
+    class: 'assault',
+    character: 'ballistic',
+    desc: 'Refined Gunslinger',
+    imgUrl: 'ballistic.jpg',
+}, 
+{
+    id: 4,
+    class: 'assault',
+    character: 'fuse',
+    desc: 'Bombastic Explosives Expert',
+    imgUrl: 'fuse.jpg',
+},
+{
+    id: 5,
+    class: 'assault',
+    character: 'mad maggie',
+    desc: 'rebel warlord',
+    imgUrl: 'mad maggie.jpg',
+},
+{
+    id: 6,
+    class: 'skirmisher',
+    character: 'horizon',
+    desc: 'Gravitational Manipulator',
+    imgUrl: 'horizon.jpg',
+},
+{
+    id: 7,
+    class: 'skirmisher',
+    character: 'octane',
+    desc: 'High-Speed Daredevil',
+    imgUrl: 'octane.jpg',
+},
+{
+    id: 8,
+    class: 'skirmisher',
+    character: 'pathfinder',
+    desc: 'Forward Scout',
+    imgUrl: 'pathfinder.jpg',
+},
+{
+    id: 9,
+    class: 'skirmisher',
+    character: 'revenant',
+    desc: 'Synthetic Nightmare',
+    imgUrl: 'revenant.jpg',
+},
+{
+    id: 10,
+    class: 'skirmisher',
+    character: 'Valkyrie',
+    desc: 'Winged Avenger',
+    imgUrl: 'valkyrie.jpg',
+},
+{
+    id: 11,
+    class: 'skirmisher',
+    character: 'wraith',
+    desc: 'Interdimensional Skirmisher',
+    imgUrl: 'wraith.jpg',
+},
+{
+    id: 12,
+    class: 'recon',
+    character: 'bloodhound',
+    desc: 'Technological Tracker',
+    imgUrl: 'bloodhound.jpg',
+},
+{
+    id: 13,
+    class: 'recon',
+    character: 'crypto',
+    desc: '',
+    imgUrl: '',
+},
+{
+    id: 14,
+
+},
+{
+    id: 15,
+
+},
+{
+    id: 16,
+
+    }
+]
